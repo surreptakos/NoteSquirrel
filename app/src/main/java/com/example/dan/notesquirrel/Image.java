980086cd7912c07da3b6b983f76b96086ced8457
surreptakos <djgatsakos@gmail.com>
2015-08-21 23:40:51 -0500

    Getting Touch Coordinates

--- app/src/main/java/com/example/dan/notesquirrel/Image.java
+++ app/src/main/java/com/example/dan/notesquirrel/Image.java
@@ -2,17 +2,43 @@ package com.example.dan.notesquirrel;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 
-public class Image extends ActionBarActivity {
+public class Image extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+        addTouchListener();
     }
+
+    private void addTouchListener() {
+        ImageView image = (ImageView) findViewById(R.id.touch_image);
+
+        image.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getX();
+                float y = event.getY();
+
+                String message = String.format("Coordinates: (%.2f, %.2f)", x, y);
+
+                Log.d(MainActivity.DEBUGTAG, message);
+
+                return false;
+            }
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
