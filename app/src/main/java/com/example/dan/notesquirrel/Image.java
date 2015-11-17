d6ce772e4c50c039bd20b948cb80a9b5b09fe911
surreptakos <djgatsakos@gmail.com>
2015-11-17 15:13:20 -0300

    Reconstructed everything using cave of programming's source code

--- app/src/main/java/com/example/dan/notesquirrel/Image.java
+++ app/src/main/java/com/example/dan/notesquirrel/Image.java
@@ -5,8 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -18,8 +21,12 @@ import java.util.List;
 
 public class Image extends AppCompatActivity implements PointCollectorListener {
 
-    private static final String PASSWORD_SET = "PASSWORD_SET";
+    public static final String RESET_PASSPOINTS = "ResetPasspoints";
+    public static final String RESET_IMAGE = "ResetImage";
+    private final static String PASSWORD_SET = "PasswordSet";
+    private final static String CURRENT_IMAGE = "CurrentImage";
     private static final int MAX_DIST = 40;
+    private boolean doPasspointReset = false;
     private PointCollector pointCollector = new PointCollector();
     private Database db = new Database(this);
 
@@ -31,30 +38,65 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
         addTouchListener();
 
-        Bundle extras = getIntent().getExtras();
-
-        if (extras != null) {
-            Boolean resetPasspoints = extras.getBoolean(MainActivity.RESET_PASSPOINTS);
-
-            if (resetPasspoints) {
-                resetPasspoints();
-            }
-        }
-        //party tiem
+        pointCollector.setListener(this);
 
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
         Boolean passpointsSet = prefs.getBoolean(PASSWORD_SET, false);
 
-        Log.d(MainActivity.DEBUGTAG, "Value of passpointsSet: " + passpointsSet);
+        String newImage = null;
 
-        if (!passpointsSet) {
-            showSetPasspointsPrompt();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+
+            // See if a flag was passed to this intent to indicate
+            // that the user wants to change the passpoint sequence.
+            doPasspointReset = extras.getBoolean(RESET_PASSPOINTS);
+
+            // See if an image path was passed to this intent,
+            // indicating that the user wants to change the image.
+            newImage = extras.getString(RESET_IMAGE);
         }
 
-        pointCollector.setListener(this);
 
+        if (newImage == null) {
+            // If the user didn't specify a new image via the reset image
+            // option, check to see if we've got an image stored which
+            // we should use.
+            newImage = prefs.getString(CURRENT_IMAGE, null);
+        } else {
+            // If the user has specified a new image to be used,
+            // save it in the preferences.
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString(CURRENT_IMAGE, newImage);
+            editor.commit();
+        }
+
+        // Set the current image. If newImage is null, this will
+        // display the default.
+        setImage(newImage);
+
+        if (!passpointsSet || doPasspointReset) {
+            showSetPasspointsPrompt();
+        } else {
+            showLoginPrompt();
+        }
+
+
+    }
+
+    private void setImage(String path) {
+
+        ImageView imageView = (ImageView) findViewById(R.id.touch_image);
+
+        if (path == null) {
+            Drawable image = ContextCompat.getDrawable(this, R.drawable.image_default);
+            imageView.setImageDrawable(image);
+        } else {
+            imageView.setImageURI(Uri.parse(path));
+        }
 
     }
 
@@ -68,8 +110,8 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
             }
         });
 
-        builder.setTitle("Create your PassPoint Sequence");
-        builder.setMessage("Touch four points on the image to set your passpoint sequence. You will need to click these same points in the future to access NoteSquirrel.");
+        builder.setTitle(R.string.create_passpoints);
+        builder.setMessage(R.string.create_passpoints_text);
 
         AlertDialog dlg = builder.create();
 
@@ -78,12 +120,28 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
 
     }
 
+    private void showLoginPrompt() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.setTitle(R.string.enter_passpoints_title);
+        builder.setMessage(R.string.enter_passpoints_text);
+
+        AlertDialog dlg = builder.create();
+
+        dlg.show();
+    }
+
     private void addTouchListener() {
         ImageView image = (ImageView) findViewById(R.id.touch_image);
 
         image.setOnTouchListener(pointCollector);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,23 +165,9 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
         return super.onOptionsItemSelected(item);
     }
 
-    private void resetPasspoints() {
-
-        Toast.makeText(Image.this, R.string.passpointsResetSuccessfully, Toast.LENGTH_LONG).show();
-
-        db.resetPoints();
-
-        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean(PASSWORD_SET, false);
-        editor.commit();
-
-
-    }
-
     private void savePasspoints(final List<Point> points) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(getString(R.string.storing_data));
+        builder.setMessage(getString(R.string.saving_passpoints));
 
         final AlertDialog dlg = builder.create();
         dlg.show();
@@ -138,7 +182,7 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
                 }
 
                 db.storePoints(points);
-                Log.d(MainActivity.DEBUGTAG, "Points saved " + points.size());
+                Log.d(MainActivity.DEBUGTAG, "Points saved " + db.getPoints().size());
 
                 return null;
             }
@@ -162,7 +206,7 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
 
     private void verifyPasspoints(final List<Point> touchedPoints) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(R.string.verifying_passpoints);
+        builder.setMessage(R.string.checking_passpoints);
 
         final AlertDialog dlg = builder.create();
         dlg.show();
@@ -207,7 +251,7 @@ public class Image extends AppCompatActivity implements PointCollectorListener {
                     Intent i = new Intent(Image.this, MainActivity.class);
                     startActivity(i);
                 } else {
-                    Toast.makeText(Image.this, R.string.Access_Denied, Toast.LENGTH_LONG).show();
+                    Toast.makeText(Image.this, R.string.access_denied, Toast.LENGTH_LONG).show();
                 }
 
             }
--- app/src/main/java/com/example/dan/notesquirrel/MainActivity.java
+++ app/src/main/java/com/example/dan/notesquirrel/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.dan.notesquirrel;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +18,7 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
@@ -23,8 +28,9 @@ public class MainActivity extends AppCompatActivity {
     public static final String DEBUGTAG = "DJG";
     public static final String TEXTFILE = "notesquirrel.txt";
     public static final String FILESAVED = "FileSaved";
-    public static final String RESET_PASSPOINTS = "ResetPasspoints";
-
+    private static final int PHOTO_TAKEN_REQUEST = 0;
+    private static final int BROWSE_GALLERY_REQUEST = 1;
+    private Uri image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +43,24 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
         if (prefs.getBoolean(FILESAVED, false)) {
             loadSavedFile();
-        } else {
-            Toast.makeText(MainActivity.this, getString(R.string.NoTextSaved), Toast.LENGTH_LONG).show();
         }
 
+    }
+
+    /*
+     * This method invokes the Image activity, telling it to get the user to
+	 * enter new passpoints. If an image uri is passed, the Image activity
+	 * should also reset the image to the given image.
+	 */
+    private void resetPasspoints(Uri image) {
+        Intent i = new Intent(this, Image.class);
+        i.putExtra(Image.RESET_PASSPOINTS, true);
+
+        if (image != null) {
+            i.putExtra(Image.RESET_IMAGE, image.getPath());
+        }
+
+        startActivity(i);
     }
 
     private void loadSavedFile() {
@@ -53,11 +73,11 @@ public class MainActivity extends AppCompatActivity {
 
             while ((line = reader.readLine()) != null) {
                 editText.append(line);
-                editText.append(getString(R.string.NewLineChar));
+                editText.append("\n");
             }
             fis.close();
         } catch (Exception e) {
-            Toast.makeText(MainActivity.this, getString(R.string.UnableToReadFile), Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, getString(R.string.toast_cant_load), Toast.LENGTH_LONG).show();
         }
 
     }
@@ -68,29 +88,44 @@ public class MainActivity extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editText = (EditText) findViewById(R.id.text);
-
-                String text = editText.getText().toString();
-
-                try {
-
-                    FileOutputStream fos = openFileOutput(TEXTFILE, Context.MODE_PRIVATE);
-                    fos.write(text.getBytes());
-                    fos.close();
-
-                    SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putBoolean(FILESAVED, true);
-                    editor.commit();
-
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, getString(R.string.toast_cant_save), Toast.LENGTH_LONG).show();
-                }
+                saveText();
 
 
             }
         });
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        saveText();
+    }
+
+    private void saveText() {
+        EditText editText = (EditText) findViewById(R.id.text);
+
+        String text = editText.getText().toString();
+
+        try {
+            FileOutputStream fos = openFileOutput(TEXTFILE,
+                    Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+
+            // Save a "file saved" preference so that next time
+            // the application runs, we'll know that this
+            // file has been saved and we have to load it.
+            SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(FILESAVED, true);
+            editor.commit();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.toast_cant_save), Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     private void addLockButtonListener() {
@@ -105,6 +140,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void replaceImage() {
+
+        // Offer a choice of methods to replace the image in a dialog;
+        // the user can either take a photo or browse the gallery.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View v = getLayoutInflater().inflate(R.layout.replace_image, null);
+        builder.setTitle(R.string.replace_lock_image);
+        builder.setView(v);
+
+        final AlertDialog dlg = builder.create();
+        dlg.show();
+
+        Button takePhoto = (Button) dlg.findViewById(R.id.take_photo);
+        Button browseGallery = (Button) dlg.findViewById(R.id.browse_gallery);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Invoke the camera activity.
+                takePhoto();
+            }
+        });
+
+        browseGallery.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Browse the gallery.
+                browseGallery();
+            }
+        });
+    }
+
+    private void browseGallery() {
+        //here we go
+
+    }
+
+    private void takePhoto() {
+        // Figure out where to put the photo when it's taken.
+        File picturesDirectory = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File imageFile = new File(picturesDirectory, "passpoints_image");
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Tell the activity to save the photo to the given file.
+        // (it will also be added to the gallery)
+        image = Uri.fromFile(imageFile);
+        i.putExtra(MediaStore.EXTRA_OUTPUT, image);
+        startActivityForResult(i, PHOTO_TAKEN_REQUEST);
     }
 
     @Override
@@ -122,9 +207,10 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.menu_passpoints_reset:
-                Intent i = new Intent(this, Image.class);
-                i.putExtra(RESET_PASSPOINTS, true);
-                startActivity(i);
+                resetPasspoints(null);
+                return true;
+            case R.id.menu_replace_image:
+                replaceImage();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
